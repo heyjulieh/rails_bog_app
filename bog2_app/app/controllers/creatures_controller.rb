ed356fd2caa990169c8c6c65f401d8fbd1f5717e
heyjulieh <heyjulieh@gmail.com>
2017-05-22 08:17:24 -0700

    finished up run 2, 2.5 hours

--- bog2_app/app/controllers/creatures_controller.rb
+++ bog2_app/app/controllers/creatures_controller.rb
@@ -13,7 +13,22 @@ class CreaturesController < ApplicationController
     end
   end
   def show
-    creature_id = params [:id]
+    creature_id = params[:id]
     @creature = Creature.find_by_id(creature_id)
+  end
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+  def update
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+private
+  def creature_params
+  creature_params = params.require(:creature).permit(:name, :description)
   end
 end
